ead31d63304e41a1664073b53df873265da4ff19
jinhyuck <gjwlsgur4866@gmail.com>
2023-06-22 15:07:57 +0900

    Feat : add chapter 1

---
+++ src/chapter1/Calculator.java
@@ -0,0 +1,30 @@
+package chapter1;
+
+public class Calculator {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public Long add(long num1, long num2) {
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public Long subtract(long num1, long num2) {
+        return num1 - num2;
+    }
+
+    public int multiple(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("분모는 0이 될 수 없습니다.");
+        }
+        return numerator / denominator;
+    }
+}
