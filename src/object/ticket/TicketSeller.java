3dbd0067ae39d221aefda064bcf785fb7677a108
jinhyuck <gjwlsgur4866@gmail.com>
2023-07-24 16:21:29 +0900

    Feat : TicketSeller, TicketOffice trade-off 관계에 대한 선택
    TicketSeller 객체가 ticketOffice 객체의 자율성 저해 vs TicketOffice 객체가 Audience 객체를 의존하여 결합도 증가
    
    두 방식 중 전자를 선택

--- src/object/ticket/TicketSeller.java
+++ src/object/ticket/TicketSeller.java
@@ -7,6 +7,9 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
+    // TicketSeller가 ticketOffice 객체의 상태를 변경시킴 -> 객체의 자율성 저해
+    // 객체의 자율성을 지키려면 TicketOffice가 Audience를 알아야 함 -> 객체간 결합도 증가
+    // Trade-off 관계가 발생 -> 결합도를 낮추는 것을 선택함
     public void sellTo(Audience audience) {
         ticketOffice.plusAmount(audience.buyTicket(ticketOffice.getTicket()));
     }
